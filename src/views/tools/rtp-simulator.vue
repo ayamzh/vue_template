ecd912364b94f0a754f04289e2292dfb8df7648e
zheng.ma <zheng.ma@centurygame.com>
2024-07-19 16:57:01 +0800

    增加占比列表

--- src/views/tools/rtp-simulator.vue
+++ src/views/tools/rtp-simulator.vue
@@ -132,6 +132,19 @@
               </el-descriptions-item>
             </el-descriptions>
             <el-divider />
+            <div style="margin-top: 20px;">
+              <el-table :data="item.result.BankruptcySection" style="width: 100%">
+                <el-table-column label="破产区间">
+                  <template slot-scope="scope">[{{ scope.row.MinLevel }} , {{ scope.row.MaxLevel }}]</template>
+                </el-table-column>
+                <el-table-column prop="PlayerCount" label="人数" />
+                <el-table-column prop="PlayerCountPercent" label="占比" />
+                <el-table-column prop="PlayerCountPercentTotal" label="累计占比" />
+                <el-table-column prop="PlayerCountLackPercent" label="1-累计占比" />
+                <el-table-column prop="SpinTimes" label="spin次数" />
+                <el-table-column prop="RTP" label="RTP" />
+              </el-table>
+            </div>
             <div :id="'bankruptcyPercent' + item.id" :ref="'bankruptcyPercent' + item.id" />
           </el-collapse-item>
         </el-collapse>
